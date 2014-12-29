ba454f8bea09ddb629721da72aee824ea2ac1de1
daulys <daulys2@gmail.com>
2014-12-28 20:03:21 -0500

    add GA
    add GA

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+header ("Location: http://www.gug.pe");
+?>
+
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>I.E. Peruano Suizo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="GUG">
+    <meta name="author" content="GUG">
+    <meta name="email" content="gdglima@gmail.com">
+    
+  <!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55838618-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+ 
+  <body>
+  </body>
+</html>
